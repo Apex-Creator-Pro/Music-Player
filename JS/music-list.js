b308ad19f40a5cbae078f87e3a0ffbc2958ca137
Clever Programmer <151522977+Apex-Creator-Pro@users.noreply.github.com>
2024-03-25 16:21:16 +0530

    Add files via upload

---
+++ JS/music-list.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+      name: "Arcade",
+      artist: "Duncane Laurance",
+      img: "assets/media/img-1.png",
+      src: "assets/songs/Arcade.mp3.mp3",
+    },
+    {
+      name: "Heat Waves",
+      artist: "Glass Animals",
+      img: "assets/media/img-2.png",
+      src: "assets/songs/HeatWaves.mp3.mp3",
+    },
+    {
+      name: "Gangsta's Paradise",
+      artist: "Coolio",
+      img: "assets/media/img-3.png",
+      src: "assets/songs/GangstasParadise.mp3",
+    },
+  ];
